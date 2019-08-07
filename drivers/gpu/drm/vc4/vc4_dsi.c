359d8019c6736ef50d8ccf05b903ac6daaacfd81
Boris Brezillon <boris.brezillon@collabora.com>
2019-08-07 09:34:44 +0200

    drm/bridge: Introduce drm_bridge_chain_get_next_bridge()
    And use it in drivers accessing the bridge->next field directly.
    This is part of our attempt to make the bridge chain a double-linked list
    based on the generic list helpers.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
    ---
    Changes in v2:
    * Kill the last/first helpers (they're not really needed)
    * Drop the !bridge || !bridge->encoder test

--- drivers/gpu/drm/vc4/vc4_dsi.c
+++ drivers/gpu/drm/vc4/vc4_dsi.c
@@ -1636,8 +1636,10 @@ static void vc4_dsi_unbind(struct device *dev, struct device *master,
 	struct drm_device *drm = dev_get_drvdata(master);
 	struct vc4_dev *vc4 = to_vc4_dev(drm);
 	struct vc4_dsi *dsi = dev_get_drvdata(dev);
+	struct drm_bridge *bridge;
 
-	if (dsi->encoder->bridge.next)
+	bridge = drm_bridge_chain_get_next_bridge(&dsi->encoder->bridge);
+	if (bridge)
 		pm_runtime_disable(dev);
 
 	vc4_dsi_encoder_destroy(dsi->encoder);
