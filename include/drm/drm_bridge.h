26a380704b8e1fafce186f9788280e93ddaa3e32
Boris Brezillon <boris.brezillon@collabora.com>
2019-08-07 17:22:59 +0200

    drm/bridge: Add the drm_bridge_chain_get_prev_bridge() helper
    Will be useful for bridge drivers that want to do bus format
    negotiation with their neighbours.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>

--- include/drm/drm_bridge.h
+++ include/drm/drm_bridge.h
@@ -481,6 +481,8 @@ int drm_bridge_attach(struct drm_encoder *encoder, struct drm_bridge *bridge,
 
 struct drm_bridge *
 drm_bridge_chain_get_next_bridge(struct drm_bridge *bridge);
+struct drm_bridge *
+drm_bridge_chain_get_prev_bridge(struct drm_bridge *bridge);
 bool drm_bridge_chain_mode_fixup(struct drm_bridge *bridge,
 				 const struct drm_display_mode *mode,
 				 struct drm_display_mode *adjusted_mode);
