d22d76487cdde21c7cebdf632221c62cbcae8f4e
Boris Brezillon <boris.brezillon@collabora.com>
2019-08-10 17:16:18 +0200

    drm/udl: Get rid of virtio_gpu_enc_helper_funcs
    Encoder helper funcs are optional, no need to implement stubs for
    those.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>

--- drivers/gpu/drm/udl/udl_encoder.c
+++ drivers/gpu/drm/udl/udl_encoder.c
@@ -19,37 +19,6 @@ static void udl_enc_destroy(struct drm_encoder *encoder)
 	kfree(encoder);
 }
 
-static void udl_encoder_disable(struct drm_encoder *encoder)
-{
-}
-
-static void udl_encoder_prepare(struct drm_encoder *encoder)
-{
-}
-
-static void udl_encoder_commit(struct drm_encoder *encoder)
-{
-}
-
-static void udl_encoder_mode_set(struct drm_encoder *encoder,
-				 struct drm_display_mode *mode,
-				 struct drm_display_mode *adjusted_mode)
-{
-}
-
-static void
-udl_encoder_dpms(struct drm_encoder *encoder, int mode)
-{
-}
-
-static const struct drm_encoder_helper_funcs udl_helper_funcs = {
-	.dpms = udl_encoder_dpms,
-	.prepare = udl_encoder_prepare,
-	.mode_set = udl_encoder_mode_set,
-	.commit = udl_encoder_commit,
-	.disable = udl_encoder_disable,
-};
-
 static const struct drm_encoder_funcs udl_enc_funcs = {
 	.destroy = udl_enc_destroy,
 };
@@ -64,7 +33,6 @@ struct drm_encoder *udl_encoder_init(struct drm_device *dev)
 
 	drm_encoder_init(dev, encoder, &udl_enc_funcs, DRM_MODE_ENCODER_TMDS,
 			 NULL);
-	drm_encoder_helper_add(encoder, &udl_helper_funcs);
 	encoder->possible_crtcs = 1;
 	return encoder;
 }
